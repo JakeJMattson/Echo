7cf1f9d3ef46e52ae210ad8fb0a34ef1d6d7499c
JakeJMattson <JakeJMattson@gmail.com>
2021-02-05 13:46:08 -0600

    Configure build

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,33 @@ version = "1.0-SNAPSHOT"
 
 plugins {
     kotlin("jvm") version "1.4.30"
+
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+    id("com.github.ben-manes.versions") version "0.36.0"
 }
 
 repositories {
     mavenCentral()
+    jcenter()
+    maven("https://oss.sonatype.org/content/repositories/snapshots/")
 }
 
 dependencies {
+    implementation("me.jakejmattson:DiscordKt:0.22.0-SNAPSHOT")
+    implementation("me.schlaubi:lavakord:0.3")
+}
 
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+
+    shadowJar {
+        archiveFileName.set("Echo.jar")
+        manifest {
+            attributes(
+                "Main-Class" to "me.jakejmattson.echo.MainAppKt"
+            )
+        }
+    }
 }
