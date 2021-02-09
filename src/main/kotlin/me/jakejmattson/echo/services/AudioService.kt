863d0fa88463edc105a81f9c8fc1893f10dd3e33
JakeJMattson <JakeJMattson@gmail.com>
2021-02-08 22:24:17 -0600

    Simplify connecting and playing logic

--- src/main/kotlin/me/jakejmattson/echo/services/AudioService.kt
+++ src/main/kotlin/me/jakejmattson/echo/services/AudioService.kt
@@ -9,16 +9,16 @@ import dev.kord.x.lavalink.kord.lavakord
 import dev.kord.x.lavalink.rest.TrackResponse
 import dev.kord.x.lavalink.rest.loadItem
 import kotlinx.coroutines.flow.firstOrNull
-import kotlinx.coroutines.runBlocking
 import me.jakejmattson.discordkt.api.Discord
 import me.jakejmattson.discordkt.api.annotations.Service
-import java.net.ConnectException
 
 @Service
 class AudioService(discord: Discord) {
     private val lavaKord = discord.kord.lavakord()
 
-    fun addServer() = lavaKord.addNode("ws://127.0.0.1:2333/", "password")
+    init {
+        lavaKord.addNode("ws://127.0.0.1:2333/", "password")
+    }
 
     suspend fun play(member: Member, query: String) {
         println("Playing $query from ${member.displayName}")
@@ -27,13 +27,14 @@ class AudioService(discord: Discord) {
         val player = link.player
         val search = if (query.startsWith("http")) query else "ytsearch:$query"
         val response = link.loadItem(search)
+        val track = response.track
 
-        println(response.tracks.first().info)
+        println(track.info)
 
         when (response.loadType) {
-            TrackResponse.LoadType.TRACK_LOADED -> player.playTrack(response.tracks.first())
-            TrackResponse.LoadType.PLAYLIST_LOADED -> player.playTrack(response.tracks.first())
-            TrackResponse.LoadType.SEARCH_RESULT -> player.playTrack(response.tracks.first())
+            TrackResponse.LoadType.TRACK_LOADED -> player.playTrack(track)
+            TrackResponse.LoadType.PLAYLIST_LOADED -> player.playTrack(track)
+            TrackResponse.LoadType.SEARCH_RESULT -> player.playTrack(track)
             TrackResponse.LoadType.NO_MATCHES -> println("No matches")
             TrackResponse.LoadType.LOAD_FAILED -> println(response.exception?.message ?: "Error")
         }
